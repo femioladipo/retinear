74d356b20d0ee28fc43e7a44fd1141522759ff04
ayokariks <ayokariks@gmail.com>
2019-10-27 00:20:23 +0100

    scraper update

--- packages/api/src/scarpper.js
+++ packages/api/src/scarpper.js
@@ -3,32 +3,28 @@ const IMDB_URL = (movie_id) => `https://www.imdb.com/title/${movie_id}/`;
 const MOVIE_ID = `tt6763664`;
 // scrapper function used for test
 const scrapper = async (url) => {
-  /* Initiate the Puppeteer browser */
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  /* Go to the IMDB Movie page and wait for it to load */
-  await page.goto(IMDB_URL(MOVIE_ID), { waitUntil: 'networkidle0' });
-  /* Run javascript inside of the page */
-  let data = await page.evaluate(() => {
-    let title = document.querySelector('div[class="title_wrapper"] > h1').innerText;
-    let images = Array.from(document.querySelectorAll('img')); //waits to receive url images
+    /* Initiate the Puppeteer browser */
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    /* Go to the IMDB Movie page and wait for it to load */
+    await page.goto(IMDB_URL(MOVIE_ID), { waitUntil: 'networkidle0' });
+    /* Run javascript inside of the page */
+    let data = await page.evaluate(() => {
+        let title = document.querySelector('h1').innerText;
+        let img_urls = Array.from(document.querySelectorAll('img')).map(i => i.src); //waits to receive url images
+        let text = Array.from(document.querySelectorAll('p')).map(p => p.innerText).join(", "); //waits to receive url images
 
-    for (const img of images) {
-        console.log(img);
-    }
+        /* Returning an object filled with the scraped data */
+        return {
+            title,
+            img_urls,
+            text
+        }
 
-    let srcs = images.map(i => i.src); //for every image in the array and get the image source
-
-    /* Returning an object filled with the scraped data */
-    return {
-        title,
-        srcs
-    }
-    
-  });
-  /* Outputting what we scraped */
-  console.log(data);
-  await browser.close();
+    });
+    /* Outputting what we scraped */
+    console.log(data);
+    await browser.close();
 
 };
 
