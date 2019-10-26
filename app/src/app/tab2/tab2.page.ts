b940191967d1870aaa5f0fdd2c8db546c4abba4c
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2019-10-26 21:55:44 +0100

    Added in-built browser. Works currently only on mobile devices

--- app/src/app/tab2/tab2.page.ts
+++ app/src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import {Platform} from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,15 +9,20 @@ import {Platform} from '@ionic/angular';
 })
 export class Tab2Page implements OnInit{
 
+
   constructor(private platform: Platform) {}
 
   ngOnInit(): void {
 
+  }
+
+  public grabSearch(){
     this.platform.ready().then(() => {
       const browser = new InAppBrowser();
-      browser.create('https://ionicframework.com/');
-      // browser.on("loadstop").subscribe(()=> console.log("loadstop"));
+      browser.create('https://www.google.co.uk/','_self','location=yes');
+      //browser.on("loadstop").subscribe(()=> console.log("loadstop"));
     });
+
   }
 
 }
