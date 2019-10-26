c22c246792c89789c16b4e5f34315c5f7ec6a9e9
wilfp <wilfaskins@gmail.com>
2019-10-26 17:39:23 +0100

    Add in app browser

--- app/src/app/tab2/tab2.page.ts
+++ app/src/app/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  constructor(private platform: Platform) {}
+
+  ngOnInit(): void {
+
+    this.platform.ready().then(() => {
+      const browser = new InAppBrowser();
+      browser.create('https://ionicframework.com/');
+      // browser.on("loadstop").subscribe(()=> console.log("loadstop"));
+    });
+  }
 
 }
