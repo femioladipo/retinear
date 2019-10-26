76187779f0d02731f6d1b452d6877cd0fe34b776
“Femi <oluwafemi.oladipo@ibm.com>
2019-10-26 22:04:38 +0100

    Update api

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,18 +1,25 @@
 const functions = require('firebase-functions')
 const express = require('express')
 
-const text_summary = require('./text_summary')
+const getTextSummary = require('./text_summary')
+const readingTime = require('./length')
 
 const app = express()
 
-app.post('/text_summary', async (req, res) => {
-  console.log('called')
-  const summary = await text_summary(req.body.text)
-  // optional call translate
-  return res.status(200).send(summary)
-})
+const constructFinalStatement = (websiteName, timeToRead, imageCaptions, textSummary) => `
+  You are visiting a page by ${websiteName}. 
+  It will take ${timeToRead} minutes to read.
+  It contains ${imageCaptions.length} images on with themes, ${imageCaptions.slice(2).join(',')}
+  Finally, the page is about: ${textSummary},
+`
 
-// app.post('/img_desc')
+app.post('/', async (req, res) => {
+  const { websiteName, text, img_urls } = req.body
+  const textSummary = await getTextSummary(text).then(({ output }) => output)
+  const timeToRead = readingTime(text)
+  // const imageCaptions = img_desc(img_urls)
+  return res.send(constructFinalStatement(websiteName, timeToRead, [], textSummary))
+})
 
 app.use('*', (req, res) => {
   res.status(404).send('404 welcome to the abyss!')
--- packages/api/src/length.js
+++ packages/api/src/length.js
@@ -1,9 +1,9 @@
 function readingTime(text) {
-    const wordsPerMinute = 200;
-    const noOfWords = text.split(/\s/g).length;
-    const minutes = noOfWords / wordsPerMinute;
-    const readTime = Math.ceil(minutes);
-    return `${readTime} minute read`;
-  }
-const post = 'This is a test post';
-console.log(readingTime(post))
+  const wordsPerMinute = 200;
+  const noOfWords = text.split(/\s/g).length;
+  const minutes = noOfWords / wordsPerMinute;
+  const readTime = Math.ceil(minutes);
+  return `${readTime} minute read`;
+}
+
+module.exports = exports = readingTime
