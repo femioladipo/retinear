7ff9b23756bb85dbc86347d4fd8602f535dd4588
ayokariks <ayokariks@gmail.com>
2019-10-26 22:03:48 +0100

    test scrapper

--- packages/api/src/scarpper.js
+++ packages/api/src/scarpper.js
@@ -1,14 +1,37 @@
-const http = require('http');
+const puppeteer = require('puppeteer');
+const IMDB_URL = (movie_id) => `https://www.imdb.com/title/${movie_id}/`;
+const MOVIE_ID = `tt6763664`;
+// scrapper function used for test
+const scrapper = async (url) => {
+  /* Initiate the Puppeteer browser */
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  /* Go to the IMDB Movie page and wait for it to load */
+  await page.goto(IMDB_URL(MOVIE_ID), { waitUntil: 'networkidle0' });
+  /* Run javascript inside of the page */
+  let data = await page.evaluate(() => {
+    let title = document.querySelector('div[class="title_wrapper"] > h1').innerText;
+    let images = Array.from(document.querySelectorAll('img')); //waits to receive url images
 
-const hostname = '127.0.0.1';
-const port = 3000;
+    for (const img of images) {
+        console.log(img);
+    }
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+    let srcs = images.map(i => i.src); //for every image in the array and get the image source
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    /* Returning an object filled with the scraped data */
+    return {
+        title,
+        srcs
+    }
+    
+  });
+  /* Outputting what we scraped */
+  console.log(data);
+  await browser.close();
+
+};
+
+scrapper(IMDB_URL)
+// let a = () => console.log(1)
+// a()
