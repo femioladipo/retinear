e824719c02e8f670e1429f9d315fb71eec3f9a2e
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2019-10-26 17:35:34 +0100

    Added slides and ability of dark-mode

--- app/src/app/tab3/tab3.page.ts
+++ app/src/app/tab3/tab3.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
   constructor() {}
+
+  ngOnInit() {
+    
+  }
+
+  public clickToggle(){
+    document.body.classList.toggle('--ion-color-dark');
+    document.body.classList.toggle('dark');
+  }
+
+
 
 }
