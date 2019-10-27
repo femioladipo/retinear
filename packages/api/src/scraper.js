f77b37c335fa9cc6481398adfa0edcb174a1462a
ayokariks <ayokariks@gmail.com>
2019-10-27 04:46:39 +0000

    scrapper adddition of domain name

--- packages/api/src/scraper.js
+++ packages/api/src/scraper.js
@@ -1,20 +1,31 @@
 const puppeteer = require('puppeteer');
+const parseDomain = require("parse-domain")
+
+
+
+
 
 const scrapper = async (url) => {
+    const { domain } = parseDomain(url)
+
     /* Initiate the Puppeteer browser */
     const browser = await puppeteer.launch();
+
     const page = await browser.newPage();
+
     /* Go to the IMDB Movie page and wait for it to load */
     await page.goto(url, { waitUntil: 'networkidle0' });
     /* Run javascript inside of the page */
+
     let data = await page.evaluate(() => {
         let title = document.querySelector('h1').innerText;
         let img_urls = Array.from(document.querySelectorAll('img')).map(i => i.src); //waits to receive url images
         let text = Array.from(document.querySelectorAll('p')).map(p => p.innerText).join(", "); //waits to receive url images
 
+
         /* Returning an object filled with the scraped data */
+
         return {
-            webSiteName: "",
             title,
             img_urls,
             text
@@ -23,7 +34,10 @@ const scrapper = async (url) => {
     });
     await browser.close();
 
-    return data
+    // console.log(data)
+    const result = Object.assign({ websiteName: domain }, data) //displays domain name 
+    // console.log(result)
+    return result
 };
 
 module.exports = exports = scrapper
