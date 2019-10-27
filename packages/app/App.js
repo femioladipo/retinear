7c2231934faac24122138b624da0fe48e8ad3640
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2019-10-27 08:47:09 +0000

    Added the logo and some text into new Account page

--- packages/app/App.js
+++ packages/app/App.js
@@ -2,12 +2,12 @@ import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 // import Speech from 'react-native-speech';
 
-import Settings from './components/Settings'
+import Account from './components/Account'
 import Browser from './components/Browser'
 
 const TabNavigator = createBottomTabNavigator({
     Browser,
-    Settings,
+    Account,
 });
 
 export default createAppContainer(TabNavigator);
---
+++ packages/app/components/Account.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+
+class Account extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image style={{width:500, height:350}}
+          source = {require("../assets/teamLogo.png")}
+          />
+
+          <Text>We are the Retinaear Team {"\n"}  {"\n"}
+          Our aim is to provide individuals with disabilities a better experience of Google Search {"\n"}  {"\n"}
+          We provide users with an additional perception - where the app helps them understand the websites better for a smoother browsing experience</Text>
+
+        </View>
+        
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Account
