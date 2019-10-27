7b297e2f0faef1fc036c55eeb88004849588b6d4
“Femi <oluwafemi.oladipo@ibm.com>
2019-10-27 01:39:24 +0000

    Update RN

--- rn-app/App.js
+++ rn-app/App.js
@@ -1,82 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import {StatusBar} from 'react-native';
-import Speech from 'react-native-speech';
+// import Speech from 'react-native-speech';
 
-interface IProps {
-
-}
-
-interface IState {
-    url: string;
-}
-
-class Browser extends React.Component<IProps, IState> {
-
-    constructor() {
-        super();
-        this.state = {
-            url: ""
-        };
-
-        /*Speech.speak({
-            text: 'React Native Speech, testing 123.',
-            voice: 'en-US'
-        });*/
-    }
-
-  render() {
-      return (
-          <View style={styles.container}>
-              <View style={styles.statusBar} />
-              <WebView
-              source={{ uri: 'https://google.com' }}
-              onNavigationStateChange={this.onNavChange.bind(this)}
-          />
-          </View>
-      );
-  }
-
-  onNavChange(webViewState) {
-
-      if (webViewState.url !== this.state.url) {
-          alert(webViewState.url);
-      }
-
-      this.setState({ url: webViewState.url })
-  }
-}
-
-class Settings extends React.Component {
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.statusBar} />
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Settings!</Text>
-            </View>
-            </View>
-        );
-    }
-}
+import Settings from './components/Settings'
+import Browser from './components/Browser'
 
 const TabNavigator = createBottomTabNavigator({
-    Settings,
     Browser,
-});
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    statusBar: {
-        backgroundColor: "#1D3B57",
-        height: StatusBar.currentHeight,
-    }
+    Settings,
 });
 
 export default createAppContainer(TabNavigator);
---
+++ rn-app/components/Settings.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Settings!</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    backgroundColor: "#1D3B57"
+  }
+});
+
+export default Settings
