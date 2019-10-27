ef16fcb111e9d0724d15af9d082def3d50498d13
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2019-10-27 03:24:14 +0000

    Commiting for another change

--- rn-app/App.js
+++ rn-app/App.js
@@ -4,10 +4,12 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import Dashboard from './components/Dashboard'
 import Browser from './components/Browser'
+import Account from './components/Account'
 
 const TabNavigator = createBottomTabNavigator({
     Browser,
     Settings,
+    Account,
 });
 
 export default createAppContainer(TabNavigator);
